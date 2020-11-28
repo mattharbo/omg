fb891df2139cb53a67073e743bd063442b4457af
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-11-28 15:54:12 +0100

    Nested goalevent when creating a new goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,16 +8,41 @@ class GoalsController < ApplicationController
 	def new
 		@savedgoals=Goal.where(fixture:params[:fixture])
 		@passed_fixture = Fixture.find(params[:fixture])
+		allplayers=Player.where(team:@passed_fixture.hometeam).or(Player.where(team:@passed_fixture.awayteam))
+		@players = allplayers.map do |d|
+			["("+d.shirtnumber.to_s+") "+d.name, d.id]
+		end
+		@players.unshift(nil)
 		@goal=Goal.new
+		#Following should be changed in the future for multiple goalevents created during the parent goal creation
+		@goal.goalevents.build
 	end
 
 	def create
+
+		# raise
 
 		Goal.create(
 			team:Team.find(params['goal']['team']),
 			minute:params['goal']['minute'],
 			fixture:Fixture.find(params['goal']['fixture'])
 		)
+
+		#looping into something bigger when will have multiple goalevents added at a time
+		params['goal']['goalevents_attributes'].each do |event,eventattribute|
+			Goalevent.create(
+				goal:Goal.last,
+				player:Player.find(eventattribute['player']),
+				# startdepth:eventattribute['startdepth'],
+				# startwidth:eventattribute['startwidth'],
+				enddepth:eventattribute['enddepth'],
+				endwidth:eventattribute['endwidth'],
+				eventtype:Eventtype.find(1),
+				# if eventtype is 1 then assist is 0
+				assist:0,
+				penalty:eventattribute['penalty']
+			)
+		end
 
 		totgoals=Fixture.find(params['goal']['fixture']).scorehome+Fixture.find(params['goal']['fixture']).scoreaway
 		registeredgoals=Goal.where(fixture:params['goal']['fixture']).count
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,5 @@ class Goal < ApplicationRecord
   belongs_to :fixture
   belongs_to :team
   has_many :goalevents, dependent: :destroy
+  accepts_nested_attributes_for :goalevents
 end

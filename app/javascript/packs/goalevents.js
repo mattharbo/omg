0d7120dca8c8bff61a530e1ef238ae021e363a71
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-12-10 17:42:28 +0100

    Get & save goal location in cage with JS in the goalevent new form #26 + Remove current & auto set the endgoal coords based on goal cage location #27

--- app/javascript/packs/goalevents.js
+++ app/javascript/packs/goalevents.js
@@ -1,31 +1,34 @@
 const pitch=document.getElementById("pitch");
-const lastpointer=document.querySelectorAll(".poiner");
+const cage=document.getElementById("thecage");
 
+//-------------------------
 const retrievecoords = (event) => {
 
-	if (document.getElementById("startgoal").checked) {
-		// si c'est FALSE !!!!
-		pitch.innerHTML="";
-		document.getElementById("goal_goalevents_attributes_0_startwidth").value=event.offsetX;
-		document.getElementById("goal_goalevents_attributes_0_startdepth").value=event.offsetY;
+	pitch.innerHTML="";
+	document.getElementById("goal_goalevents_attributes_0_startwidth").value=event.offsetX;
+	document.getElementById("goal_goalevents_attributes_0_startdepth").value=event.offsetY;
 
-		document.querySelectorAll(".goalcoords small")[0].innerText=`Goal start coords: ${event.offsetX}/${event.offsetY}`;
-
-		pitch.insertAdjacentHTML("beforeend", `<div class="pointer_start" 
-			style="top: ${event.offsetY-7}px;left: ${event.offsetX-7}px;"></div>`);
-
-	} else {
-		// si c'est TRUE !!
-
-		document.getElementById("goal_goalevents_attributes_0_endwidth").value=event.offsetX;
-		document.getElementById("goal_goalevents_attributes_0_enddepth").value=event.offsetY;
-
-		document.querySelectorAll(".goalcoords small")[1].innerText=`Goal end coords: ${event.offsetX}/${event.offsetY}`;
-
-		pitch.insertAdjacentHTML("beforeend", `<div class="pointer_end" 
-			style="top: ${event.offsetY-4}px;left: ${event.offsetX-4}px;"></div>`);
-	}
-	
+	pitch.insertAdjacentHTML("beforeend", `<div class="pointer_start" 
+		style="top: ${event.offsetY-7}px;left: ${event.offsetX-7}px;"></div>`);
 };
 
+//-------------------------
+const goallocation = (event) => {
+
+	cage.innerHTML="";
+
+	const goallocationonpitch = ((34/192)*event.offsetX)+155;
+
+	document.getElementById("goal_goalevents_attributes_0_enddepth").value=goallocationonpitch.toFixed(1);
+	document.getElementById("goal_goalevents_attributes_0_endwidth").value=0;
+
+	cage.insertAdjacentHTML("beforeend", `<div class="pointer_goal" 
+			style="top: ${event.offsetY-6}px;left: ${event.offsetX-6}px;"></div>`);
+
+	pitch.insertAdjacentHTML("beforeend", `<div class="pointer_end" 
+			style="top: 0px;left: ${goallocationonpitch}px;"></div>`);
+};
+
+//-------------------------
 pitch.addEventListener("click", retrievecoords);
+cage.addEventListener("click", goallocation);
